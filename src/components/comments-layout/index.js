152890f356741c28574f16cef8793a9adaf81a38
Vanilio1997 <Vlad.rabota.96@yandex.ru>
2023-06-12 06:54:43 +0300

    fixed for empty commentsfunction CommentsLayout({children}) {

--- src/components/comments-layout/index.js
+++ src/components/comments-layout/index.js
@@ -3,15 +3,12 @@ import PropTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname';
 import './style.css';
 
-function CommentsLayout({header,children}) {
+function CommentsLayout({children}) {
 
   const cn = bem('CommentsLayout');
 
   return (
     <div className={cn()}>
-      <div className={cn('header')}>
-        {header}
-      </div>
         {children}
     </div>
   );
